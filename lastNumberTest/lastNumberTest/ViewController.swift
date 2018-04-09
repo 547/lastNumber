ab6aaa87cebcc0965f2be08b0ece27b07954e77a
qiwang <timer_sevenwang@163.com>
2018-04-09 16:45:20 +0800

    update

--- lastNumberTest/lastNumberTest/ViewController.swift
+++ lastNumberTest/lastNumberTest/ViewController.swift
@@ -22,7 +22,8 @@ class ViewController: UIViewController {
     }
     @IBAction func change(_ sender: UIButton) {
         guard let number = Decimal.init(string: textField.text ?? "") else { return }
-        let result = NumberHandler.lastAdd(Decimal.init(-100), to: number)
+//        let result = NumberHandler.lastNumberAdd(Decimal.init(-100), to: number)
+        let result = number.lastNumberAdd(Decimal.init(100))
         textField.text = result.description
     }
 
@@ -33,7 +34,7 @@ class NumberHandler {
 }
 extension NumberHandler {
     ///末位数 加 几
-    static func lastAdd(_ addNumber:Decimal, to number:Decimal) -> Decimal {
+    static func lastNumberAdd(_ addNumber:Decimal, to number:Decimal) -> Decimal {
         let numberString = number.description
         
         let pointIndex = numberString.reversed().index(of: ".") ?? 0
@@ -41,6 +42,19 @@ extension NumberHandler {
         
         let largerNumber = number * multiple
         let changedNumber = largerNumber + addNumber
+        let shrinkNumber = changedNumber / multiple
+        return shrinkNumber
+    }
+}
+extension Decimal {
+    func lastNumberAdd(_ number:Decimal) -> Decimal {
+        let numberString = description
+        
+        let pointIndex = numberString.reversed().index(of: ".") ?? 0
+        let multiple = pow(Decimal.init(10), pointIndex)
+        
+        let largerNumber = self * multiple
+        let changedNumber = largerNumber + number
         let shrinkNumber = changedNumber / multiple
         return shrinkNumber
     }
